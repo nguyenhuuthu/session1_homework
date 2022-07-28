17b140b42b23a6b5f80312d419d86b5e2dbdc231
nguyenhuuthu <110140882+nguyenhuuthu@users.noreply.github.com>
2022-07-28 17:05:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+//1.trường hợp thông báo lỗi syntax error: 
+// -khai báo hai lần
+// -sử dungh kí tự đặc biệt
+// -bắt đầu bằng số
+// 2.trong javascrip làm thế nào để ktra dữ liệu:
+// - dựa vào biến
+// 3. cso bao nhiêu kiểu dữ liệu tring khai báo biến?
+// -Trong JavaScript có tất cả 7 kiểu dữ liệu chính:
+//string
+//number
+//boolean
+//null
+//object
+//undefined
+//array
+
+// a
+let msg = ("rikkei is awesome");
+console.log(msg);
+//b
+let studentCount = 0;
+console.log(studentCount );
+// 3a
+msg ="Coding is hard but super cool and fun";
+console.log(msg);
+// 3b
+studentCount = 0+30;
+console.log (studentCount);
+//cách để biến một chuỗi thành viết hoa toàn bộ và ngược lại?
+//toLowerCase() trong JavaScript là một phương thức của đối tượng String, có tác dụng chuyển tất cả chữ hoa có trong chuỗi ban đầu thành chữ thường và tạo ra một chuỗi mới. Phép chuyển chữ thường này sẽ không làm thay đổi chuỗi ban đầu.
+//toUpperCase() trong JavaScript là một phương thức của đối tượng String, có tác dụng chuyển tất cả chữ thường có trong chuỗi ban đầu thành chữ hoa và tạo ra một chuỗi mới. Phép chuyển chữ hoa này sẽ không làm thay đổi chuỗi ban đầu.
+console.log(msg.toUpperCase());
+// 3d.
+
+
+
+
+
